43e6e69f950c1ea11ed93ab770834d4b7269420f
igork <confiq@users.noreply.github.com>
2020-12-14 13:29:52 +0100

    add manager

---
+++ seedeebot/libs/basePlugin.py
@@ -0,0 +1,5 @@
+
+# inspired from here: https://packaging.python.org/guides/creating-and-discovering-plugins/
+class BasePluginV1:
+    def __init__(self):
+        pass
--- seedeebot/manager.py
+++ seedeebot/manager.py
@@ -5,6 +5,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class PluginsManager(object):
     def __init__(self):
         pass
